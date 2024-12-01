436b83feaab81b68ab79e7f8072d9d67d9b9cc55
홍준영 <hongjy1997@naver.com>
2024-12-01 16:01:43 +0900

    섹션 5 일급 컬렉션

--- src/main/java/cleancode/minesweeper/tobe/GameBoard.java
+++ src/main/java/cleancode/minesweeper/tobe/GameBoard.java
@@ -3,12 +3,10 @@ package cleancode.minesweeper.tobe;
 import cleancode.minesweeper.tobe.cell.*;
 import cleancode.minesweeper.tobe.gamelevel.GameLevel;
 import cleancode.minesweeper.tobe.position.CellPosition;
+import cleancode.minesweeper.tobe.position.CellPositions;
 import cleancode.minesweeper.tobe.position.RelativePosition;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.Random;
-import java.util.stream.Stream;
 
 public class GameBoard {
 
@@ -105,42 +103,47 @@ public class GameBoard {
 
     //보드판을 형성
     public void initializeGame() {
-        int rowSize = getRowSize();
-        int colSize = getColSize();
+        CellPositions cellPositions = CellPositions.from(board);
 
         // i j를 의미에 맞게 row 와 col으로 변경
         //보드를 생성하는 과정
-        for (int row = 0; row < rowSize; row++) {
-            for (int col = 0; col < colSize; col++) {
-                //BOARD 초기화
-                board[row][col] = new EmptyCell();
-            }
-        }
+        initializeEmptyCells(cellPositions);
+
         //의미를 갖지 않는 i값이기 때문에 그대로 냅둠
         //지뢰를 설치하는 과정
-        for (int i = 0; i < landMineCount; i++) {
-            int landMineCol = new Random().nextInt(colSize);
-            int landMineRow = new Random().nextInt(rowSize);
-            board[landMineRow][landMineCol] = new LandMineCell();
-        }
+        List<CellPosition> landMinePositions = cellPositions.extractRandomPositions(landMineCount);
+        initializeLandMineCells(landMinePositions);
+
         // i j를 의미에 맞게 row 와 col으로 변경
-        for (int row = 0; row < rowSize; row++) {
-            for (int col = 0; col < colSize; col++) {
-                CellPosition cellPosition = CellPosition.of(row, col);
-                if (isLandMineCellAt(cellPosition)) { //지뢰Cell인경우
-                    //NEARBY_LAND_MINE_COUNTS[row][col] = 0;
-                    //BOARD[row][col] = Cell.create(); 를 해줄때 0으로 세팅하기에 필요없는 코드가 됨
-                    continue;
-                }
-                //지뢰가 아닐때
-                int count = countNearbyLandMines(cellPosition); //주변 지뢰의 개수를 가져옴
-                if(count == 0){
-                    continue;
-                }
-                NumberCell numberCell = new NumberCell(count);
-                board[row][col] = numberCell;
+        List<CellPosition> numberPositionCandidates = cellPositions.subtract(landMinePositions);
+        initializeNumberCells(numberPositionCandidates);
+
+    }
+
+    private void initializeEmptyCells(CellPositions cellPositions) {
+        List<CellPosition> allPositions = cellPositions.getPositions();
+        for (CellPosition position : allPositions) {
+            updateCellAt(position, new EmptyCell());
+        }
+    }
+
+    private void initializeLandMineCells(List<CellPosition> landMinePositions) {
+        for (CellPosition position : landMinePositions) {
+            updateCellAt(position, new LandMineCell());
+        }
+    }
+
+    private void initializeNumberCells(List<CellPosition> numberPositionCandidates) {
+        for (CellPosition candidatePosition : numberPositionCandidates) {
+            int count = countNearbyLandMines(candidatePosition);
+            if (count != 0) {
+                updateCellAt(candidatePosition, new NumberCell(count));
             }
         }
+    }
+
+    private void updateCellAt(CellPosition position, Cell cell) {
+        board[position.getRowIndex()][position.getColIndex()] = cell;
     }
 
     public int getRowSize() {
@@ -154,41 +157,8 @@ public class GameBoard {
     //개인 의견으로는 깃발이 꽂혀있을때 지뢰가 있어야만 확인해야하는게 아닌가 싶음
 
     public boolean isAllCellChecked() {
-        //이중 for문 해소
-        return Arrays.stream(board)
-                .flatMap(Arrays::stream)
-                //getter를 사용하는 대신 공개메서드를 사용
-                //.noneMatch(Cell::isClosed);
-                .allMatch(Cell::isChecked);
-        /*
-            1. Arrays.stream(BOARD)
-                BOARD라는 2차원 배열을 Arrays.stream() 메서드를 사용하여 배열의 각 행(1차원 배열)을 스트림으로 변환
-
-            2. flatMap(Arrays::stream)
-                a. flatMap()은 각 요소를 처리하면서 내부의 스트림을 평탄화 한다
-                예) String[][] BOARD = {
-                        {"A", "B", "C"},
-                        {"D", "E", "F"},
-                        {"G", "H", "I"}
-                    };
-                    배열이 있을때 Arrays.stream(BOARD)를 실행하면
-
-                    Stream<String[]> // 각 행 {"A", "B", "C"}, {"D", "E", "F"}, {"G", "H", "I"}
-
-                    이런식으로 만들어지게 되고 이걸 한번더 flatMap을 하게 되면
-
-                    Stream<String> // "A", "B", "C", "D", "E", "F", "G", "H", "I"
-
-                    1차원 스트림으로 변환하고 하나의 스트림으로 병합한다.
-
-                    물건이 여러 칸에 나누어 담겨 있는 서랍을 하나로 합치는 것으로 비유할 수 있다.
-                b. Arrays::stream은 각 1차원 배열(행)을 다시 스트림으로 변환한다.
-                c. flatMap의 결과로 전체 2차원 배열이 1차원 스트림으로 변환하게 된다.
-            3. noneMatch(CLOSED_CELL_SIGN::equals)
-                a. noneMatch는 스트림의 모든 요소에 대해 조건을 검사하여, *조건을 만족하는 요소가 하나도 없을경우* true를 반환한다.
-                b. CLOSED_CELL_SIGN::equals 조건을 사용하고 있으며,
-                    각 셀이 CLOSED_CELL_SIGN과 같은지 확인 하고 같은 값이 하나도 없으면 true, 그렇지 않으면 false를 반환한다
-         */
+        Cells cells = Cells.from(board);
+        return cells.isAllChecked();
     }
 
     public String getSign(CellPosition cellPosition){
